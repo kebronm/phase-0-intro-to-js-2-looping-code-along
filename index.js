2554104efa3c60438d16310e5af075d4a0ecc87c
Kebron Mehari <kebronmehari400@gmail.com>
2022-03-26 12:06:33 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+const birthday = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(birthday) {
+  for (let i = 0; i < birthday.length; i++) {
+    
+    console.log(`Thank you ${birthday[i]} for the wonderful surprise gift!,`);
+    
+    debugger;
+  }
+
+  return birthday;
+}
+writeCards(birthday);
+
+function countDown(){;
+
+    let countDown = 1;
+    while (countDown < 12) {
+      debugger;
+      if (countDown === 1) {
+        console.log(`This loop has executed ${countDown} time.`);
+      } else {
+        console.log(`This loop has executed ${countDown} times.`);
+      }
+      countDown += 1;
+      debugger;
+    }};
+    
+    while (countDown > 0) {
+      console.log(countDown--);
+    }
+    for (let countDown = 4; countDown > 0; countDown--) {
+      console.log(countDown);
+    }
